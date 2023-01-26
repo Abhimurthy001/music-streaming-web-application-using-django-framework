758956026afc29a76aba934bf680709a5cd0d9e0
Abhi murthy <90611136+Abhimurthy001@users.noreply.github.com>
2023-01-26 12:20:04 +0530

    Update views.py

--- musicbeats/views.py
+++ musicbeats/views.py
@@ -33,12 +33,12 @@ def watchlater(request):
         
         for i in watch:
             if video_id == i.video_id:
-                message = "Your Video is Already Added"
+                message = "Your song is Already Added"
                 break
         else:
             watchlater = Watchlater(user=user, video_id=video_id)
             watchlater.save()
-            message = "Your Video is Succesfully Added"
+            message = "Your song is Succesfully Added"
 
         song = Song.objects.filter(song_id=video_id).first()
         return render(request, f"musicbeats/songpost.htm", {'song': song, "message": message})
